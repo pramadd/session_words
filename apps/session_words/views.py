6fa35c760a980c6effa3d8551f05182a34a40d02
Pranathi <neethu@codingdojo.com>
2017-11-13 20:52:40 -0800

    2nd commit

--- apps/session_words/views.py
+++ apps/session_words/views.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
 from django.utils.crypto import get_random_string
@@ -6,12 +6,20 @@ from time import gmtime, strftime
 
 
 def index(request):
+  # for i in request.session['inputs']:
+  #     print i['word']
+  #     print i['color']
+  #     print i['font']
+  #     print i['color']
   if 'word' not in request.session:
       request.session['word']="yyy"
   if 'color' not in request.session:
       request.session['color'] = "kkk"
   if 'font' not in request.session:
       request.session['font'] = "gg"
+  if 'inputs' not in request.session:
+      print "we're here"
+      request.session['inputs'] = []
 
   return render(request,'session_words/index.html')
 
@@ -25,17 +33,28 @@ def process(request):
         else:
           print "false"
           request.POST["font"] == False
-   
-          request.session['inputs'] = request.session['inputs']
+  
           temp = {
           'word':request.POST['word'],
           'color':request.POST['color'],
           'font':request.POST['font'], 
           'time':strftime("%H:%M:%S%p, %B %d, %Y", gmtime())
           }
-        request.session['inputs'].append(temp)
+        temp2 = request.session['inputs']
+        temp2.append(temp)
+        request.session['inputs'] = temp2
+
+        # request.session['inputs'].append(temp)
+        # for i in request.session['inputs']:
+        #   print "yarrrr"
+        #   print i['word']
+        #   print i['color']
+        #   print i['font']
+          
         return redirect('/')
+
 def clear(request):
+  print "clearing"
   request.session['inputs'] = []
   return redirect('/')
 
